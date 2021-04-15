f75f7b2c25c7d881f168d419c1310b295588aced
henanewind <zzwonder2009@126.com>
2021-04-15 20:19:04 +0800

    + 上传vue测试代码  npm intall

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type='text' v-model='message'>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  data () {
+    return {
+      message: '我的第一个Vue应用...'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
